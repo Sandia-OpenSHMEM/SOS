6aeece6f5d21391b9b35598a988485d644a7c9c4
bwbarre <bwbarre@3ed51f3f-9ec5-d088-cf24-7cb058622c1e>
2011-01-28 18:02:25 +0000

    shell of SHMEM over Portals4

---
+++ trunk/src/runtime.c
@@ -0,0 +1,26 @@
+/* -*- C -*-
+ *
+ * Copyright (c) 2011 Sandia National Laboratories. All rights reserved.
+ */
+
+#include "config.h"
+#include "shmem.h"
+
+int
+_num_pes(void)
+{
+    return 0;
+}
+
+
+int
+_my_pe(void)
+{
+    return 0;
+}
+
+
+void
+start_pes(int npes)
+{
+}
