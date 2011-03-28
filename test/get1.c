7055d9c7c740ce86783362870a9a152b61c3b902
stan.smith <stan.smith@3ed51f3f-9ec5-d088-cf24-7cb058622c1e>
2011-03-28 23:34:39 +0000

    Strided put tests along with additional basics.

---
+++ test/get1.c
@@ -0,0 +1,37 @@
+#include <mpp/shmem.h>
+
+#include <string.h>
+#include <stdio.h>
+
+static long source[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+static long target[10];
+
+int
+main(int argc, char* argv[])
+{
+    int i;
+
+    start_pes(0);
+
+    if (_my_pe() == 0) {
+	memset(target, 0, sizeof(target));
+        /* put 10 elements into target on PE 1 */
+        shmem_long_get(target, source, 10, 1);
+    }
+
+    shmem_barrier_all();  /* sync sender and receiver */
+
+    if (_my_pe() == 0) {
+        if (0 != memcmp(source, target, sizeof(long) * 10)) {
+            fprintf(stderr,"[%d] Src & Target mismatch?\n",_my_pe());
+            for (i = 0 ; i < 10 ; ++i) {
+                printf("%ld,%ld ", source[i], target[i]);
+            }
+            printf("\n");
+            return 1;
+        }
+    }
+    shmem_barrier_all();
+
+    return 0;
+}
---
+++ test/iput_longdouble.c
@@ -0,0 +1,50 @@
+/*
+ * adaptation of example from SGI man page for shmem_iput.
+ */
+
+#include <stdio.h>
+#include <mpp/shmem.h>
+#define _IPUT(a) shmem_##a##_iput
+
+//#define IPUT _IPUT(double)
+#define IPUT shmem_longdouble_iput
+#define DataType long double
+
+static DataType source[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+static DataType target[10];
+
+int main(int argc, char **argv)
+{
+	int me, nProcs, rc=0;
+
+	start_pes(0);
+	me = _my_pe();
+	nProcs = _num_pes();
+
+	if (me == 0) {
+		int j;
+		/* put 5 words into target on PE's [1 to (nProcs-1)] */
+		for(j=1; j < nProcs; j++)
+			IPUT (target, source, 1, 2, 5, j);
+	}
+
+	shmem_barrier_all(); /* sync sender and receiver */
+
+	if (me != 0) {
+		if (target[0] != 1 ||
+		    target[1] != 3 ||
+		    target[2] != 5 ||
+		    target[3] != 7 ||
+		    target[4] != 9)
+		{
+			printf("ERR: target on PE %d is %Lf %Lf %Lf %Lf %Lf\n"
+				"  Expected 1,3,5,7,9?\n",
+				me, target[0], target[1], target[2],
+				target[3], target[4] );
+			rc = 1;
+		}
+	}
+	shmem_barrier_all(); /* sync before exiting */
+
+	return rc;
+}
