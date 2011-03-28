66c525a5372400680465c50402846f89d006a39a
stan.smith <stan.smith@3ed51f3f-9ec5-d088-cf24-7cb058622c1e>
2011-03-28 23:33:01 +0000

    Announce conditions when failing. sync prior to exit.

--- trunk/test/put1.c
+++ trunk/test/put1.c
@@ -5,19 +5,22 @@
 int
 main(int argc, char* argv[])
 {
-    long source[10] = { 1, 2, 3, 4, 5,
-                        6, 7, 8, 9, 10 };
+    long source[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
     static long target[10];
 
     start_pes(0);
+
     if (_my_pe() == 0) {
         /* put 10 elements into target on PE 1 */
         shmem_long_put(target, source, 10, 1);
     }
+
     shmem_barrier_all();  /* sync sender and receiver */
+
     if (_my_pe() == 1) {
         if (0 != memcmp(source, target, sizeof(long) * 10)) {
             int i;
+            fprintf(stderr,"[%d] Src & Target mismatch?\n",_my_pe());
             for (i = 0 ; i < 10 ; ++i) {
                 printf("%ld,%ld ", source[i], target[i]);
             }
@@ -25,6 +28,10 @@ main(int argc, char* argv[])
             return 1;
         }
     }
+
+    shmem_barrier_all();
+
+    //fprintf(stderr,"[%d] exit\n",_my_pe());
 
     return 0;
 }
