94410dd11096067ae56deae727e358b3f91ef2f4
bwbarre <bwbarre@3ed51f3f-9ec5-d088-cf24-7cb058622c1e>
2011-03-29 21:09:11 +0000

    Implement the symmetric heap

--- trunk/src/symmetric_heap.c
+++ trunk/src/symmetric_heap.c
@@ -7,6 +7,7 @@
 
 #include <portals4.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "mpp/shmem.h"
 #include "shmem_internal.h"
@@ -15,6 +16,25 @@ long malloc_error = 0;
 
 void *shmem_heap_base = NULL;
 long shmem_heap_length = 0;
+char *shmem_heap_curr = NULL;
+
+void* shmem_get_next(int incr);
+void* dlmalloc(size_t);
+void  dlfree(void*);
+void* dlrealloc(void*, size_t);
+void* dlmemalign(size_t, size_t);
+
+void*
+shmem_get_next(int incr)
+{
+    char *orig = shmem_heap_curr;
+    shmem_heap_curr += (incr > 0) ? incr : 0;
+    if (shmem_heap_curr - (char*) shmem_heap_base > shmem_heap_length) {
+        printf("WARNING: top of symmetric heap found\n");
+        return (void*) -1;
+    }
+    return orig;
+}
 
 int
 symmetric_init(void)
@@ -24,39 +44,35 @@ symmetric_init(void)
     if (NULL != env) req_len = atoi(env);
 
     shmem_heap_length = req_len;
-    shmem_heap_base = malloc(shmem_heap_length);
+    shmem_heap_base = shmem_heap_curr = malloc(shmem_heap_length);
     if (NULL == shmem_heap_base)  return -1;
 
-    /* BWB: Unimplemented */
     return 0;
 }
 
 void *
 shmalloc(size_t size)
 {
-    /* BWB: Unimplemented */
-    return NULL;
+    return dlmalloc(size);
 }
 
 
 void
 shfree(void *ptr)
 {
-    /* BWB: Unimplemented */
+    dlfree(ptr);
 }
 
 
 void *
 shrealloc(void *ptr, size_t size)
 {
-    /* BWB: Unimplemented */
-    return NULL;
+    return dlrealloc(ptr, size);
 }
 
 
 void *
 shmemalign(size_t alignment, size_t size)
 {
-    /* BWB: Unimplemented */
-    return NULL;
+    return dlmemalign(alignment, size);
 }
