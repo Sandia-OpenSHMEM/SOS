eadfce1a969d9f30b7467e7202c3d2a354e712fd
bwbarre <bwbarre@3ed51f3f-9ec5-d088-cf24-7cb058622c1e>
2011-03-29 22:17:00 +0000

    Fix some comments, fix shmem_ptr and addr_accessible

--- src/memory.c
+++ src/memory.c
@@ -13,41 +13,47 @@
 void
 shmem_clear_cache_inv(void)
 {
+    /* Intentionally a no-op */
 }
 
 
 void
 shmem_set_cache_inv(void)
 {
+    /* Intentionally a no-op */
 }
 
 
 void
 shmem_clear_cache_line_inv(void *target)
 {
+    /* Intentionally a no-op */
 }
 
 
 void
 shmem_set_cache_line_inv(void *target)
 {
+    /* Intentionally a no-op */
 }
 
 
 void
 shmem_udcflush(void)
 {
+    /* Intentionally a no-op */
 }
 
 
 void
 shmem_udcflush_line(void *target)
 {
+    /* Intentionally a no-op */
 }
 
 
 void *
 shmem_ptr(void *target, int pe)
 {
-    return target;
+    return NULL;
 }
