12c087670e6f2aa6d4fd21b20ce2b7f2967818e9
stan.smith <stan.smith@3ed51f3f-9ec5-d088-cf24-7cb058622c1e>
2011-09-08 17:10:39 +0000

    Correctly support up to 21 PEs.

--- test/iput-iget.c
+++ test/iput-iget.c
@@ -1,5 +1,6 @@
 /*
  * adaptation of example from SGI man page for shmem_iput.
+ * Enhanced to support 2..21 PEs.
  */
 
 #include <stdio.h>
@@ -7,9 +8,12 @@
 
 #include <mpp/shmem.h>
 
+#define MAX_PE 21
+#define WRDS 5
+
 short source[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 static short target[10];
-static short results[15];
+static short results[MAX_PE*WRDS];
 
 int
 main(int argc, char **argv)
@@ -19,12 +23,16 @@ main(int argc, char **argv)
     start_pes(0);
     me = _my_pe();
     nProcs = _num_pes();
+    if (nProcs > MAX_PE) {
+        if (me == 0)
+            printf("%s ERR: Too many PE, Max @ %d\n",argv[0], MAX_PE);
+        return(1);
+    }
 
     if (me == 0) {
         /* put 5 words into target on PE's [1 to (nProcs-1)] */
-        for (j=1; j < nProcs; j++) {
-            shmem_short_iput(target, source, 1, 2, 5, j);
-        }
+        for(j=1; j < nProcs; j++)
+            shmem_short_iput(target, source, 1, 2, WRDS, j);
     }
 
     shmem_barrier_all(); /* sync sender and receiver */
@@ -35,13 +43,14 @@ main(int argc, char **argv)
             target[1] != 3 ||
             target[2] != 5 ||
             target[3] != 7 ||
-            target[4] != 9) {
-                printf("ERR: target on PE %d is %hd %hd %hd %hd %hd\n"
-                       "  Expected 1,3,5,7,9?\n",
-                       me, target[0], target[1], target[2],
-                       target[3], target[4] );
-                rc = 1;
-            }
+            target[4] != 9)
+        {
+            printf("%s ERR: target on PE %d is %hd %hd %hd %hd %hd\n"
+                "  Expected 1,3,5,7,9?\n", argv[0],
+                me, target[0], target[1], target[2],
+                target[3], target[4] );
+            rc = 1;
+        }
     }
 
     shmem_barrier_all();
@@ -50,30 +59,34 @@ main(int argc, char **argv)
         return rc;
 
     if (me == 0) {
-        // Pull data from PE1
-        // iget() Procs target[0...4] --> results[Proc*5 ...Proc*5+5]
-        // short_iget() == short_get() in the following case.
-        //shmem_short_iget(&results[k], target, 1, 1, 5, 1);
-        shmem_short_get(results, target, 10, 1);
-        shmem_barrier_all();
+        // Pull data from PE1...(nProcs-1)
+        // iget() nProcs target[0...4] --> results[Proc*WRDS ...Proc*WRDS+WRDS]
 
-        for (j=1,k=0; j < nProcs; j++,k+=5) {
+        for(j=1,k=0; j < nProcs; j++,k+=WRDS)
+            shmem_short_iget(&results[k], target, 1, 1, WRDS, j);
+
+        for(j=1,k=0; j < nProcs; j++,k+=WRDS) {
             if (results[k] != 1 ||
                 results[k+1] != 3 ||
                 results[k+2] != 5 ||
                 results[k+3] != 7 ||
                 results[k+4] != 9)
-                {
-                    printf("[%d] ERR: results %hd %hd %hd %hd %hd\n"
-                           "  Expected 1,3,5,7,9?\n",
-                           me, results[k], results[k+1], results[k+2],
-                           results[k+3], results[k+4] );
-                    rc = 1;
-                }
+            {
+                printf("%s [%d] ERR: results %hd %hd %hd %hd %hd\n"
+                    "  Expected 1,3,5,7,9?\n", argv[0],
+                    me, results[k], results[k+1], results[k+2],
+                    results[k+3], results[k+4] );
+                rc = 1;
+            }
         }
-    }  else {
-        shmem_barrier_all();
     }
+#if _DEBUG
+    else {
+        printf("%s [%d] target is %hd %hd %hd %hd %hd\n", argv[0],
+                me, target[0], target[1], target[2],
+                target[3], target[4] );
+    }
+#endif
 
     shmem_barrier_all(); /* sync before exiting */
 
