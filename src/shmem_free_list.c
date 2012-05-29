4afaf912422d0dd3fdd92fcab2064e03b0412191
bwbarrett <bwbarrett@gmail.com>
2012-05-29 03:15:21 +0000

    * Add free list and atomics implementations

---
+++ src/shmem_free_list.c
@@ -0,0 +1,88 @@
+/* -*- C -*-
+ *
+ * Copyright 2011 Sandia Corporation. Under the terms of Contract
+ * DE-AC04-94AL85000 with Sandia Corporation, the U.S.  Government
+ * retains certain rights in this software.
+ * 
+ * This file is part of the Portals SHMEM software package. For license
+ * information, see the LICENSE file in the top level directory of the
+ * distribution.
+ *
+ */
+
+#include "config.h"
+
+#include "mpp/shmem.h"
+#include "shmem_internal.h"
+#include "shmem_free_list.h"
+
+
+shmem_free_list_t*
+shmem_free_list_init(unsigned int element_size,
+                     shmem_free_list_item_init_fn_t init_fn)
+{
+    int ret;
+    shmem_free_list_t *fl = (shmem_free_list_t*) calloc(1, sizeof(shmem_free_list_t));
+    if (NULL == fl) return NULL;
+
+    fl->element_size = element_size;
+    fl->init_fn = init_fn;
+    shmem_spinlock_init(&fl->lock);
+    ret = shmem_free_list_more(fl);
+    if (0 != ret) return NULL;
+
+    return fl;
+}
+
+
+void
+shmem_free_list_destroy(shmem_free_list_t *fl)
+{
+    shmem_free_list_alloc_t *alloc, *next;
+
+    alloc = fl->allocs;
+    while (NULL != alloc) {
+        next = alloc->next;
+        free(alloc);
+        alloc = next;
+    }
+
+    shmem_spinlock_fini(&fl->lock);
+}
+
+
+int
+shmem_free_list_more(shmem_free_list_t *fl)
+{
+    int real_element_size = fl->element_size + sizeof(shmem_free_list_item_t);
+    int page_size = 4096 - sizeof(shmem_free_list_alloc_t);
+    int num_elements = (real_element_size < page_size) ? page_size / real_element_size : 1;
+    shmem_free_list_item_t *item, *first, *last = NULL;
+    shmem_free_list_alloc_t *header;
+    char *buf;
+    int i;
+
+    buf = malloc(sizeof(shmem_free_list_alloc_t) + 
+                 num_elements * real_element_size);
+    if (NULL == buf) return 1;
+
+    header = (shmem_free_list_alloc_t*) buf;
+    first = item = (shmem_free_list_item_t*) (header + 1);
+    for (i = 0 ; i < num_elements ; ++i, item++) {
+        fl->init_fn(item);
+        if (i == num_elements - 1) {
+            item->next = NULL;
+            last = item;
+        } else {
+            item->next = (item + 1);
+        }
+    }
+
+    header->next = fl->allocs;
+    fl->allocs = header;
+
+    if (NULL != last) last->next = fl->head;
+    fl->head = first;
+
+    return 0;
+}
---
+++ src/shmem_free_list.h
@@ -0,0 +1,84 @@
+/* -*- C -*-
+ *
+ * Copyright 2011 Sandia Corporation. Under the terms of Contract
+ * DE-AC04-94AL85000 with Sandia Corporation, the U.S.  Government
+ * retains certain rights in this software.
+ * 
+ * This file is part of the Portals SHMEM software package. For license
+ * information, see the LICENSE file in the top level directory of the
+ * distribution.
+ *
+ */
+
+#ifndef SHMEM_FREE_QUEUE_H
+#define SHMEM_FREE_QUEUE_H
+
+#include <pthread.h>
+
+#include "shmem_atomic.h"
+
+struct shmem_free_list_item_t {
+    struct shmem_free_list_item_t *next;
+};
+typedef struct shmem_free_list_item_t shmem_free_list_item_t;
+
+struct shmem_free_list_alloc_t {
+    struct shmem_free_list_alloc_t *next;
+};
+typedef struct shmem_free_list_alloc_t shmem_free_list_alloc_t;
+
+typedef void (*shmem_free_list_item_init_fn_t)(shmem_free_list_item_t *item);
+
+struct shmem_free_list_t {
+    uint32_t element_size;
+
+    shmem_free_list_item_init_fn_t init_fn;
+    shmem_free_list_alloc_t *allocs;
+    shmem_free_list_item_t* head;
+    shmem_spinlock_t lock;
+};
+typedef struct shmem_free_list_t shmem_free_list_t;
+
+shmem_free_list_t* shmem_free_list_init(unsigned int element_size, 
+                                        shmem_free_list_item_init_fn_t init_fn);
+void shmem_free_list_destroy(shmem_free_list_t *fl);
+int shmem_free_list_more(shmem_free_list_t *fl);
+
+
+static inline
+void*
+shmem_free_list_alloc(shmem_free_list_t *fl)
+{
+    shmem_free_list_item_t *item = NULL;
+    int ret;
+
+    shmem_spinlock_lock(&fl->lock);
+    if (NULL == fl->head) {
+        ret = shmem_free_list_more(fl);
+        if (0 != ret) goto done;
+    }
+    assert(NULL != fl->head);
+
+    item = fl->head;
+    fl->head = item->next;
+
+ done:
+    shmem_spinlock_unlock(&fl->lock);
+
+    return item;
+}
+
+
+static inline
+void
+shmem_free_list_free(shmem_free_list_t *fl, void *data)
+{
+    shmem_free_list_item_t *item = (shmem_free_list_item_t*) data;
+    
+    shmem_spinlock_lock(&fl->lock);
+    item->next = fl->head;
+    fl->head = item;
+    shmem_spinlock_unlock(&fl->lock);
+}
+
+#endif
