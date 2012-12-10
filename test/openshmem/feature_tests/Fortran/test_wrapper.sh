7af01a2fbc75816c72b5a21c7ac4399a513469d6
bwbarrett <bwbarrett@gmail.com>
2012-12-10 02:01:05 +0000

    Finish adding the Fortran tests to the test suite.  Need a wrapper script to parse the output, since OpenSHMEM's tests just print errors instead of using STOP.

---
+++ test/openshmem/feature_tests/Fortran/test_wrapper.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+a=`$*`
+b=$?
+echo "$a"
+if test $b -eq 0 ; then
+  if test "`echo $a | grep 'more PE'`" != "" ; then
+    exit 77
+  elif test "`echo $a | grep Passed`" != "" ; then
+    exit 0
+  else
+    exit 1
+  fi
+else
+  exit $b
+fi
