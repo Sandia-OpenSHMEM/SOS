af37661022b5d5236bac1d5eb97dda92b2e6005b
bwbarrett@gmail.com <bwbarrett@gmail.com@3ed51f3f-9ec5-d088-cf24-7cb058622c1e>
2012-12-10 02:24:22 +0000

    more skip keywords

--- trunk/test/openshmem/feature_tests/Fortran/test_wrapper.sh
+++ trunk/test/openshmem/feature_tests/Fortran/test_wrapper.sh
@@ -6,6 +6,8 @@ echo "$a"
 if test $b -eq 0 ; then
   if test "`echo $a | grep 'more PE'`" != "" ; then
     exit 77
+  elif test "`echo $a | grep 'skipped'`" != "" ; then
+    exit 77
   elif test "`echo $a | grep Passed`" != "" ; then
     exit 0
   else
