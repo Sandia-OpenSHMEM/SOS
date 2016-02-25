91f5acdabac13dc91a77d40b025bf985e82c55e2
James Dinan <james.dinan@intel.com>
2016-02-24 22:38:37 -0500

    OFI: Update MR_SCALABLE to register heap/data segs
    Update the MR_SCALABLE path to separately register heap and data segment
    and use offset-based addressing, rather than registering all memory and
    using absolute virtual addressing.  This patch improves performance and
    memory consuption at scale when MR_SCALABLE is used without remote
    virtual addressing.
    
    Signed-off-by: James Dinan <james.dinan@intel.com>

--- src/transport_ofi.h
+++ src/transport_ofi.h
@@ -39,18 +39,14 @@ extern struct fid_cq*              	shmem_transport_ofi_put_nb_cqfd;
 extern struct fid_cntr*            	shmem_transport_ofi_target_cntrfd;
 extern struct fid_cntr*            	shmem_transport_ofi_put_cntrfd;
 extern struct fid_cntr*            	shmem_transport_ofi_get_cntrfd;
-#ifdef ENABLE_MR_SCALABLE
-extern struct fid_mr*              	shmem_transport_ofi_target_mrfd;
-#else
-extern struct fid_mr*                   shmem_transport_ofi_target_heap_mrfd;
-extern struct fid_mr*                   shmem_transport_ofi_target_data_mrfd;
+#ifndef ENABLE_MR_SCALABLE
 extern uint64_t*                        shmem_transport_ofi_target_heap_keys;
 extern uint64_t*                        shmem_transport_ofi_target_data_keys;
-#endif /* ENABLE_MR_SCALABLE */
 #ifndef ENABLE_REMOTE_VIRTUAL_ADDRESSING
 extern uint8_t**                       shmem_transport_ofi_target_heap_addrs;
 extern uint8_t**                       shmem_transport_ofi_target_data_addrs;
 #endif /* ENABLE_REMOTE_VIRTUAL_ADDRESSING */
+#endif /* ENABLE_MR_SCALABLE */
 extern uint64_t          		shmem_transport_ofi_pending_put_counter;
 extern uint64_t 	       	 	shmem_transport_ofi_pending_get_counter;
 extern uint64_t				shmem_transport_ofi_pending_cq_count;
@@ -68,22 +64,23 @@ extern size_t    			shmem_transport_ofi_bounce_buffer_size;
 static inline
 void shmem_transport_ofi_get_mr(const void *addr, int dest_pe,
                                 uint8_t **mr_addr, uint64_t *key) {
-    *key = 0;
 #ifdef ENABLE_REMOTE_VIRTUAL_ADDRESSING
+    *key = 0;
     *mr_addr = (uint8_t*) addr;
 #else
     if ((void*) addr >= shmem_internal_data_base &&
         (uint8_t*) addr < (uint8_t*) shmem_internal_data_base + shmem_internal_data_length) {
 
-        *mr_addr = shmem_transport_ofi_target_data_addrs[dest_pe] +
-            ((uint8_t *) addr - (uint8_t *) shmem_internal_data_base);
+        *key = 0;
+        *mr_addr = (uint8_t*) ((uint8_t *) addr - (uint8_t *) shmem_internal_data_base);
 
     } else if ((void*) addr >= shmem_internal_heap_base &&
               (uint8_t*) addr < (uint8_t*) shmem_internal_heap_base + shmem_internal_heap_length) {
 
-        *mr_addr = shmem_transport_ofi_target_heap_addrs[dest_pe] +
-            ((uint8_t *) addr - (uint8_t *) shmem_internal_heap_base);
+        *key = 1;
+        *mr_addr = (uint8_t*) ((uint8_t *) addr - (uint8_t *) shmem_internal_heap_base);
     } else {
+        *key = 0;
         *mr_addr = NULL;
         printf("[%03d] ERROR in %s: address (0x%p) outside of symmetric areas\n",
                shmem_internal_my_pe, __func__, addr);
