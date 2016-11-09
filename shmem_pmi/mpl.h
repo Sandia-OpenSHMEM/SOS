b098a214c3d64153db48bae728bb9291ad8e54a9
James Dinan <james.dinan@intel.com>
2016-11-09 10:03:27 -0500

    Cleanup the MPL header harness for PMI
    Signed-off-by: James Dinan <james.dinan@intel.com>

--- shmem_pmi/mpl.h
+++ shmem_pmi/mpl.h
@@ -1,5 +1,20 @@
-#ifndef SOS_MPL
-#define SOS_MPL
+/* -*- C -*-
+ *
+ * Copyright 2016 Sandia Corporation. Under the terms of Contract
+ * DE-AC04-94AL85000 with Sandia Corporation, the U.S.  Government
+ * retains certain rights in this software.
+ *
+ * Copyright (c) 2016 Intel Corporation. All rights reserved.
+ * This software is available to you under the BSD license.
+ *
+ * This file is part of the Sandia OpenSHMEM software package. For license
+ * information, see the LICENSE file in the top level directory of the
+ * distribution.
+ *
+ */
+
+#ifndef MPL_H
+#define MPL_H
 
 #include <string.h>
 #include <stdio.h>
@@ -8,24 +23,24 @@
 #define ATTRIBUTE __attribute__
 #define MPI_MAX_PORT_NAME 256
 
-#define MPL_snprintf(...)     \
-    ({ snprintf(__VA_ARGS__); \
-        0;                    \
+#define MPL_snprintf(...)       \
+    ({ snprintf(__VA_ARGS__);   \
+       0;                       \
     })
 
-#define MPL_strncpy(...)                                \
-    ({ strncpy(__VA_ARGS__);                            \
-        0;                                              \
+#define MPL_strncpy(...)        \
+    ({ strncpy(__VA_ARGS__);    \
+       0;                       \
     })
 
-#define MPL_exit     exit
+#define MPL_exit exit
 
-#define MPL_strnapp(...)                    \
-    ({ strncat(__VA_ARGS__);                \
-        0;                                  \
+#define MPL_strnapp(...)        \
+    ({ strncat(__VA_ARGS__);    \
+       0;                       \
     })
 
 #define MPL_internal_error_printf(...) \
-    fprintf(stderr, __VA_ARGS__);
+    do { fprintf(stderr, __VA_ARGS__); } while(0)
 
-#endif /* SOS_MPL */
+#endif /* MPL_H */
