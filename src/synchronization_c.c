4097f7477f987d2e799270aaa144784a2143d3a2
James Dinan <james.dinan@intel.com>
2016-11-18 17:52:08 -0500

    Generate profiling goop for synch API
    Signed-off-by: James Dinan <james.dinan@intel.com>

--- src/synchronization_c.c
+++ src/synchronization_c.c
@@ -20,47 +20,7 @@
 #include "shmem_internal.h"
 #include "shmem_atomic.h"
 #include "shmem_synchronization.h"
-
-#ifdef ENABLE_PROFILING
-#include "pshmem.h"
-
-#pragma weak shmem_quiet = pshmem_quiet
-#define shmem_quiet pshmem_quiet
-
-#pragma weak shmem_fence = pshmem_fence
-#define shmem_fence pshmem_fence
-
-#pragma weak shmem_short_wait = pshmem_short_wait
-#define shmem_short_wait pshmem_short_wait
-
-#pragma weak shmem_short_wait_until = pshmem_short_wait_until
-#define shmem_short_wait_until pshmem_short_wait_until
-
-#pragma weak shmem_int_wait = pshmem_int_wait
-#define shmem_int_wait pshmem_int_wait
-
-#pragma weak shmem_int_wait_until = pshmem_int_wait_until
-#define shmem_int_wait_until pshmem_int_wait_until
-
-#pragma weak shmem_long_wait = pshmem_long_wait
-#define shmem_long_wait pshmem_long_wait
-
-#pragma weak shmem_long_wait_until = pshmem_long_wait_until
-#define shmem_long_wait_until pshmem_long_wait_until
-
-#pragma weak shmem_longlong_wait = pshmem_longlong_wait
-#define shmem_longlong_wait pshmem_longlong_wait
-
-#pragma weak shmem_longlong_wait_until = pshmem_longlong_wait_until
-#define shmem_longlong_wait_until pshmem_longlong_wait_until
-
-#pragma weak shmem_wait = pshmem_wait
-#define shmem_wait pshmem_wait
-
-#pragma weak shmem_wait_until = pshmem_wait_until
-#define shmem_wait_until pshmem_wait_until
-
-#endif /* ENABLE_PROFILING */
+#include "synchronization_c_pshmem.h"
 
 void
 shmem_quiet(void)
