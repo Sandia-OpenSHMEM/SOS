01ac5f24943a86d1f46c21fa1260a22698adc0fb
James Dinan <james.dinan@intel.com>
2017-01-06 20:07:37 -0500

    Add transport-none to Makefile
    Signed-off-by: James Dinan <james.dinan@intel.com>

---
+++ src/transport_none.c
@@ -0,0 +1,15 @@
+/* -*- C -*-
+ *
+ * Copyright (c) 2016 Intel Corporation. All rights reserved.
+ * This software is available to you under the BSD license.
+ *
+ * This file is part of the Sandia OpenSHMEM software package. For license
+ * information, see the LICENSE file in the top level directory of the
+ * distribution.
+ *
+ */
+
+#include "transport_none.h"
+
+void *SHMEMX_CTX_DEFAULT = NULL;
+void *SHMEMX_DOMAIN_DEFAULT = NULL;
