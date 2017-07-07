ad79210f165b6841e4e2533d0179b0ab5f0be1c6
angelali4096 <qingyanl@andrew.cmu.edu>
2017-07-07 15:30:19 -0400

    Adds folder with manpages and installation support
    Adds autotools files to support manpage generation.  The man pages are included
    in the "man" directory.  Also adds a Python script to "scripts" directory that
    can take in individual files or a folder containing LaTex files from the
    OpenSHMEM specification and converts them to manpages.
    
    Signed-off-by: angelali4096 <qingyanl@andrew.cmu.edu>

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,19 @@
-#! /bin/sh
+#! /bin/bash
 
 test -d ./config || mkdir ./config
+
+FILES=./man/*.1
+printf "dist_man1_MANS = " > ./man/Makefile.am
+for f in $FILES
+do
+  printf "%s " $(basename $f) >> ./man/Makefile.am
+done
+
+FILES=./man/*.3
+printf "\ndist_man3_MANS = " >> ./man/Makefile.am
+for f in $FILES
+do
+  printf "%s " $(basename $f) >> ./man/Makefile.am
+done
 
 autoreconf -vif
