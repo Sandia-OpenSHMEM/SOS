b3d5b3cac3c6c054e1e63c39950a25a0bdc796e0
James Dinan <james.dinan@intel.com>
2017-12-05 10:28:50 -0500

    Improve CQ usage documentation
    Signed-off-by: James Dinan <james.dinan@intel.com>

--- src/transport_ofi.c
+++ src/transport_ofi.c
@@ -996,9 +996,11 @@ static int shmem_transport_ofi_ctx_init(shmem_transport_ctx_t *ctx, int id)
         cntr_get_attr.wait_obj = FI_WAIT_UNSPEC;
     }
 
+    /* Allow provider to choose CQ size, since we are using FI_RM_ENABLED.
+     * Context format is used to return bounce buffer pointers in the event
+     * so that they can be inserted back into to the free list. */
     struct fi_cq_attr cq_attr = {0};
     cq_attr.format = FI_CQ_FORMAT_CONTEXT;
-    cq_attr.size = 0; /* Size chosen by provider */
 
     struct fabric_info* info = &shmem_transport_ofi_info;
     info->p_info->ep_attr->tx_ctx_cnt = FI_SHARED_CONTEXT;
