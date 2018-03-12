d59dc81c78f351317ef7e734b0151decacbccb63
David M. Ozog <david.m.ozog@gmail.com>
2018-03-12 16:54:55 -0400

    Add env var to disable private contexts in OFI
    This adds an environment variable that disallows private contexts from
    claiming exclusive access to an STX in the OFI layer.  This may improve
    performance for multi-threaded apps that use more exclusive private
    contexts than available STXs.
    
    Signed-off-by: David M. Ozog <david.m.ozog@intel.com>

--- src/transport_ofi.c
+++ src/transport_ofi.c
@@ -359,6 +359,17 @@ static shmem_transport_ofi_stx_kvs_t* shmem_transport_ofi_stx_kvs = NULL;
         DEBUG_MSG("STX[%ld] = [ %s ]\n", shmem_transport_ofi_stx_max, stx_str);                 \
     } while (0)
 
+static int shmem_transport_ofi_disable_private;
+
+static inline
+int shmem_transport_ofi_check_private(long options, int disable) {
+    if (!disable && (options & SHMEM_CTX_PRIVATE)) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
 /* This uses a slightly modified version of the Fisher-Yates shuffle algorithm
  * (or Knuth Shuffle).  It selects a random element from the so-far
  * unselected subset of the STX pool.  The top_idx should be reset before each
@@ -476,7 +487,7 @@ void shmem_transport_ofi_stx_allocate(shmem_transport_ctx_t *ctx)
 {
     /* SHMEM contexts that are private to the same thread (i.e. have
      * SHMEM_CTX_PRIVATE option set) share the same STX.  */
-    if (ctx->options & SHMEM_CTX_PRIVATE) {
+    if (shmem_transport_ofi_check_private(ctx->options, shmem_transport_ofi_disable_private)) {
 
         shmem_transport_ofi_stx_kvs_t *f;
         HASH_FIND_INT(shmem_transport_ofi_stx_kvs, &ctx->tid, f);
@@ -1337,7 +1348,7 @@ static int shmem_transport_ofi_ctx_init(shmem_transport_ctx_t *ctx, int id)
 
     /* Allocate STX from the pool */
     if (shmem_internal_thread_level > SHMEM_THREAD_FUNNELED &&
-        ctx->options & SHMEM_CTX_PRIVATE) {
+        shmem_transport_ofi_check_private(ctx->options, shmem_transport_ofi_disable_private)) {
             ctx->tid = shmem_transport_ofi_gettid();
     }
     shmem_transport_ofi_stx_allocate(ctx);
@@ -1430,6 +1441,12 @@ int shmem_transport_init(void)
     } else {
         RAISE_WARN_MSG("Ignoring bad STX share algorithm '%s', using 'round-robin'\n", type);
         shmem_transport_ofi_stx_allocator = ROUNDROBIN;
+    }
+
+    if (shmem_internal_params.OFI_STX_DISABLE_PRIVATE) {
+        shmem_transport_ofi_disable_private = 1;
+    } else {
+        shmem_transport_ofi_disable_private = 0;
     }
 
     /* Allocate STX array with max length */
@@ -1571,7 +1588,7 @@ void shmem_transport_ctx_destroy(shmem_transport_ctx_t *ctx)
     }
 
     if (ctx->stx_idx >= 0) {
-        if (ctx->options & SHMEM_CTX_PRIVATE) {
+        if (shmem_transport_ofi_check_private(ctx->options, shmem_transport_ofi_disable_private)) {
             shmem_transport_ofi_stx_kvs_t *e;
             HASH_FIND_INT(shmem_transport_ofi_stx_kvs, &ctx->tid, e);
             if (e) {
@@ -1634,7 +1651,8 @@ int shmem_transport_fini(void)
 
     for (i = 0; i < shmem_transport_ofi_contexts_len; ++i) {
         if (shmem_transport_ofi_contexts[i]) {
-            if (shmem_transport_ofi_contexts[i]->options & SHMEM_CTX_PRIVATE)
+            if (shmem_transport_ofi_check_private(shmem_transport_ofi_contexts[i]->options,
+                                                  shmem_transport_ofi_disable_private))
                 RAISE_WARN_MSG("Shutting down with unfreed private context (%zd)\n", i);
             shmem_transport_quiet(shmem_transport_ofi_contexts[i]);
             shmem_transport_ctx_destroy(shmem_transport_ofi_contexts[i]);
