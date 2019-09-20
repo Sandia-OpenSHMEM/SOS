ff30922e2d957ea4d145edf69421f5e3de66be6b
David M. Ozog <david.m.ozog@intel.com>
2019-09-20 11:54:06 -0400

    Copyright cleanup in teams files
    Signed-off-by: David M. Ozog <david.m.ozog@intel.com>

--- src/shmem_team.c
+++ src/shmem_team.c
@@ -1,9 +1,5 @@
 /* -*- C -*-
  *
- * Copyright 2011 Sandia Corporation. Under the terms of Contract
- * DE-AC04-94AL85000 with Sandia Corporation, the U.S.  Government
- * retains certain rights in this software.
- *
  * Copyright (c) 2019 Intel Corporation. All rights reserved.
  * This software is available to you under the BSD license.
  *
