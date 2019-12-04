f95cbf97c73007560a3f2a9ae2f0fa4e90f19262
Md <md.rahman@intel.com>
2019-12-04 15:07:19 -0600

    Keeping local changes to context texts in spec-example to allow c99 compile/runs and other checks
    Signed-off-by: Md <md.rahman@intel.com>

--- test/spec-example/shmem_ctx.c
+++ test/spec-example/shmem_ctx.c
@@ -40,14 +40,24 @@ long tasks_done = 0; /* Tasks done by this PE */
 long total_done = 0; /* Total tasks done by all PEs */
 
 int main(void) {
-    int tl, i;
+    int tl, i, ret;
     long ntasks = 1024;  /* Total tasks per PE */
 
     for (i = 0; i < SHMEM_REDUCE_SYNC_SIZE; i++)
         psync[i] = SHMEM_SYNC_VALUE;
 
-    shmem_init_thread(SHMEM_THREAD_MULTIPLE, &tl);
-    if (tl != SHMEM_THREAD_MULTIPLE) shmem_global_exit(1);
+    ret = shmem_init_thread(SHMEM_THREAD_MULTIPLE, &tl);
+
+    if (tl != SHMEM_THREAD_MULTIPLE || ret != 0) {
+        printf("Init failed (requested thread level %d, got %d, ret %d)\n",
+               SHMEM_THREAD_MULTIPLE, tl, ret);
+
+        if (ret == 0) {
+            shmem_global_exit(1);
+        } else {
+            return ret;
+        }
+    }
 
     int me = shmem_my_pe();
     int npes = shmem_n_pes();
@@ -59,29 +69,32 @@ int main(void) {
         int ret = shmem_ctx_create(SHMEM_CTX_PRIVATE, &ctx);
 
         if (ret != 0) {
-            printf("%d: Error creating context (%d)\n", me, ret);
-            shmem_global_exit(2);
+            printf("%d: Warning, unable to create context (%d)\n", me, ret);
+            ctx = SHMEM_CTX_DEFAULT;
         }
 
         /* Process tasks on all PEs, starting with the local PE.  After
          * all tasks on a PE are completed, help the next PE. */
         while (pes_done < npes) {
-            long task = shmem_atomic_fetch_inc(ctx, &task_cntr, task_pe);
+            long task = shmem_ctx_long_atomic_fetch_inc(ctx, &task_cntr, task_pe);
             while (task < ntasks) {
                 /* Perform task (task_pe, task) */
                 tasks_done++;
-                task = shmem_atomic_fetch_inc(ctx, &task_cntr, task_pe);
+                task = shmem_ctx_long_atomic_fetch_inc(ctx, &task_cntr, task_pe);
             }
             pes_done++;
             task_pe = (task_pe + 1) % npes;
         }
 
-        shmem_ctx_destroy(ctx);
+        if (ctx != SHMEM_CTX_DEFAULT) shmem_ctx_destroy(ctx);
     }
 
     shmem_long_sum_to_all(&total_done, &tasks_done, 1, 0, 0, npes, pwrk, psync);
 
     int result = (total_done != ntasks * npes);
+    if (me == 0 && result)
+        printf("Error: total_done is %ld, expected %ld\n", total_done, ntasks * npes);
+
     shmem_finalize();
     return result;
 }
--- test/spec-example/shmem_ctx_pipelined_reduce.c
+++ test/spec-example/shmem_ctx_pipelined_reduce.c
@@ -51,23 +51,27 @@ int main(void) {
 
     int ret_0 = shmem_ctx_create(0, &ctx[0]);
     int ret_1 = shmem_ctx_create(0, &ctx[1]);
-    if (ret_0 || ret_1) shmem_global_exit(1);
+
+    if (ret_0) ctx[0] = SHMEM_CTX_DEFAULT;
+    if (ret_1) ctx[1] = SHMEM_CTX_DEFAULT;
 
     for (i = 0; i < LEN; i++) {
         in_buf[i] = me; out_buf[i] = 0;
     }
 
-    int p_idx = 0, p = 0; /* Index of ctx and pbuf (p_idx) for current pipeline stage (p) */
+    /* Index of ctx and pbuf (p_idx) for current pipeline stage (p) */
+    int p_idx = 0, p = 0;
     for (i = 1; i <= npes; i++)
-        shmem_put_nbi(ctx[p_idx], &pbuf[p_idx][PLEN*me], &in_buf[PLEN*p],
-                      PLEN, (me+i) % npes);
+        shmem_ctx_int_put_nbi(ctx[p_idx], &pbuf[p_idx][PLEN*me],
+                              &in_buf[PLEN*p], PLEN, (me+i) % npes);
 
-    /* Issue communication for pipeline stage p, then accumulate results for stage p-1 */
+    /* Issue communication for pipeline stage p, then accumulate results for
+     * stage p-1 */
     for (p = 1; p < LEN/PLEN; p++) {
         p_idx ^= 1;
         for (i = 1; i <= npes; i++)
-            shmem_put_nbi(ctx[p_idx], &pbuf[p_idx][PLEN*me], &in_buf[PLEN*p],
-                          PLEN, (me+i) % npes);
+            shmem_ctx_int_put_nbi(ctx[p_idx], &pbuf[p_idx][PLEN*me],
+                                  &in_buf[PLEN*p], PLEN, (me+i) % npes);
 
         shmem_ctx_quiet(ctx[p_idx^1]);
         shmem_sync_all();
@@ -82,6 +86,15 @@ int main(void) {
         for (j = 0; j < PLEN; j++)
             out_buf[PLEN*(p-1)+j] += pbuf[p_idx][PLEN*i+j];
 
+    int errors = 0;
+    int expected = (npes - 1) * npes / 2;
+    for (i = 0; i < LEN; i++) {
+        if (out_buf[i] != expected) {
+            printf("%d: out_buf[%d] = %d, expected %d\n", me, i, out_buf[i], expected);
+            errors++;
+        }
+    }
+
     shmem_finalize();
-    return 0;
+    return errors;
 }
