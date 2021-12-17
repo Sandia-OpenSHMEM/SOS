a24833b01bf3afd2a2fa8953e6d37bf75f33133d
Md Rahman <md.rahman@intel.com>
2021-12-17 10:31:44 -0600

    Script to check profiling symbols
    Signed-off-by: Md Rahman <md.rahman@intel.com>

---
+++ scripts/pshmem_symbol_check.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# Simple script to check for missing profiling symbols in 
+# SHMEM library.
+#
+# Example usage: pshmem_symbol_check.sh <path to SHMEM library>
+#
+# Exit if any command fails
+set -e
+
+if [ $# != 1 ] ; then
+    echo "Usage: $0 SHMEM_LIBRARY"
+    exit 1
+fi
+
+SHMEM_LIB=$1
+
+readelf --syms --wide $SHMEM_LIB | grep 'shmem_' | awk '{print $8}' > symbols
+cat symbols | sort | uniq -d > shmem_symbols
+
+grep -R "pshmem_" shmem_symbols > pshmem_symbols_uniq
+echo "Number of pshmem symbols found: `wc -l pshmem_symbols_uniq | awk '{print $1}'`"
+
+sed -n '/^pshmem_/d' shmem_symbols
+
+for i in $(cat ./pshmem_symbols_uniq)
+do
+  search_str=$(echo $i | cut -c 2-)
+  sed "/${search_str}/d" -i ./shmem_symbols
+done
+
+rm -rf symbols
+rm -rf pshmem_symbols_uniq
+
+echo "APIs that dont have profiling symbols:"
+echo "--------------------------------------"
+cat shmem_symbols
+
+rm -rf shmem_symbols
