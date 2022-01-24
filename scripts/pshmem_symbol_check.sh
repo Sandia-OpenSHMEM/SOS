f3dc04cd99f08d6182c0c919ac68d3698b92c1f5
Md Rahman <md.rahman@intel.com>
2022-01-24 12:24:43 -0600

    Added a check for grep in case profiling is not enabled

--- scripts/pshmem_symbol_check.sh
+++ scripts/pshmem_symbol_check.sh
@@ -19,8 +19,14 @@ SHMEM_LIB=$1
 readelf --syms --wide $SHMEM_LIB | grep 'shmem_' | awk '{print $8}' > symbols
 cat symbols | sort | uniq -d > shmem_symbols
 
-grep -R "pshmem_" shmem_symbols > pshmem_symbols_uniq
-echo "Number of pshmem symbols found: `wc -l pshmem_symbols_uniq | awk '{print $1}'`"
+if grep -q -R "pshmem_" shmem_symbols
+then
+  grep -R "pshmem_" shmem_symbols > pshmem_symbols_uniq
+  echo "Number of pshmem symbols found: `wc -l pshmem_symbols_uniq | awk '{print $1}'`"
+else
+  echo "No PSHMEM symbols found"
+  exit 0
+fi
 
 sed -n '/^pshmem_/d' shmem_symbols
 
