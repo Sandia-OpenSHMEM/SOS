5722b9afb04453ebce03c558d4268968282defcf
Md Rahman <md.rahman@intel.com>
2023-03-29 11:16:25 -0500

    Updated unit test with an array of data for verification

--- test/shmemx/shmemx_malloc_onepe.c
+++ test/shmemx/shmemx_malloc_onepe.c
@@ -26,17 +26,19 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <shmem.h>
 #include <shmemx.h>
 
 int main(int argc, char **argv) {
     int ret = 0;
 
-    int onepe_data_size = 1;
+    int onepe_data_size = 10;
     int *onepe_alloc = NULL;
+    int i;
 
     static ptrdiff_t onepe_alloc_ptr_original = 0, onepe_alloc_ptr_retrieved = 0;
-    int onepe_alloc_val_pe1 = -1;
+    int *onepe_alloc_val_pe1;
 
     shmem_init();
 
@@ -45,32 +47,38 @@ int main(int argc, char **argv) {
 
     if (me == 1) {
         onepe_alloc = (int *) shmemx_malloc_onepe(onepe_data_size * sizeof(int));
-        onepe_alloc[0] = npes + 78;
+        for (i = 0; i < onepe_data_size; i++)
+            onepe_alloc[i] = npes + 78 + i;
         onepe_alloc_ptr_original = shmemx_ptrdiff_of_ptr(onepe_alloc);	
-        fprintf(stderr, "PE 1 allocated and set the onepe allocation\n");
     }
 
     shmem_barrier_all();
 
     if (me != 1) {
+        onepe_alloc_val_pe1 = (int *) malloc(onepe_data_size * sizeof(int));
+
         shmem_ptrdiff_get(&onepe_alloc_ptr_retrieved, &onepe_alloc_ptr_original, 1, 1);
         void *onepe_alloc_pe1 = shmemx_ptr_of_ptrdiff(onepe_alloc_ptr_retrieved);
-        shmem_int_get(&onepe_alloc_val_pe1, onepe_alloc_pe1, 1, 1);
-        fprintf(stderr, "PE 1's onepe allocation is retrieved by PE %d\n", me);
+        shmem_int_get(onepe_alloc_val_pe1, onepe_alloc_pe1, onepe_data_size, 1);
     }
 
     shmem_barrier_all();
 
     if (me != 1) {
-        if (onepe_alloc_val_pe1 != (npes + 78)) {
-            fprintf(stderr, "ERROR: Wrong value retrieved from one_pe allocation %d, expected = %d\n", 
-                             onepe_alloc_val_pe1, npes + 78);
-            ret = 1;
+        for (i = 0; i < onepe_data_size; i++) {
+            if (onepe_alloc_val_pe1[i] != (npes + 78 + i)) {
+                fprintf(stderr, "ERROR: Wrong value retrieved from one_pe allocation %d, expected = %d at index %d\n", 
+                                 onepe_alloc_val_pe1[i], npes + 78 + i, i);
+                ret = 1;
+                break;
+            }
         }
     }
 
     if (me == 1)
         shmemx_free_onepe(onepe_alloc);
+    else
+        free(onepe_alloc_val_pe1);
 
     shmem_finalize();
 
