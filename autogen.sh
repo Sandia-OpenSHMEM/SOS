ef91a6be5bfc9de33286a3e537089597e7289fa6
kholland <kieran.holland@intel.com>
2023-12-05 09:53:41 -0600

    Fixed autogen formatting

--- autogen.sh
+++ autogen.sh
@@ -3,9 +3,9 @@
 test -d ./config || mkdir ./config
 
 if [ ! -f test/Makefile.am ] || [ ! -f test/apps/Makefile.am ] ||
-     ! [ -f test/include/Makefile.am ] || [ ! -f test/performance/Makefile.am ] ||
-     ! [ -f test/shmemx/Makefile.am ] || ! [ -f test/spec-example/Makefile.am ] ||
-     ! [ -f test/unit/Makefile.am ];
+   [ ! -f test/include/Makefile.am ] || [ ! -f test/performance/Makefile.am ] ||
+   [ ! -f test/shmemx/Makefile.am ]  || [ ! -f test/spec-example/Makefile.am ] ||
+   [ ! -f test/unit/Makefile.am ];
 then
   test -f modules/tests-sos/autogen.sh || { echo "ERROR: The test directory is empty. Please run the following command to download the SOS tests:"; echo "git submodule update --init"; exit 1; }
 fi
