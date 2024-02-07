33cb21cf7d9b21a49fd69238053866692e10d8cc
David Ozog <david.m.ozog@intel.com>
2024-02-07 16:41:56 -0500

    tests-sos: have autogen.sh copy test-sos submodule

--- autogen.sh
+++ autogen.sh
@@ -2,12 +2,46 @@
 
 test -d ./config || mkdir ./config
 
-if [ ! -f test/Makefile.am ] || [ ! -f test/apps/Makefile.am ] ||
-   [ ! -f test/include/Makefile.am ] || [ ! -f test/performance/Makefile.am ] ||
-   [ ! -f test/shmemx/Makefile.am ]  || [ ! -f test/spec-example/Makefile.am ] ||
-   [ ! -f test/unit/Makefile.am ];
-then
-  test -f modules/tests-sos/autogen.sh || { echo "ERROR: The test directory is empty. Please run the following command to download the SOS tests:"; echo "git submodule update --init"; exit 1; }
+tests_sos_dir=modules/tests-sos
+
+script_dir="$(cd "$(dirname "$0")" && pwd)"
+current_dir="$(pwd)"
+
+# Check if the script is being run from within the root SOS source directory
+if [ "$script_dir" != "$current_dir" ]; then
+    echo "Error: Please run ./autogen.sh from the top-level directory of SOS."
+    exit 1
+fi
+
+if [ ! -d "${tests_sos_dir}" ]; then
+    echo "Error: the tests-sos submodule directory ${tests_sos_dir} does not exist"
+    exit 1
+elif [ ! -f "${tests_sos_dir}/test/Makefile.am" ] ||
+     [ ! -f "${tests_sos_dir}/test/apps/Makefile.am" ] ||
+     [ ! -f "${tests_sos_dir}/test/include/Makefile.am" ] ||
+     [ ! -f "${tests_sos_dir}/test/performance/Makefile.am" ] ||
+     [ ! -f "${tests_sos_dir}/test/shmemx/Makefile.am" ] ||
+     [ ! -f "${tests_sos_dir}/test/spec-example/Makefile.am" ] ||
+     [ ! -f "${tests_sos_dir}/test/unit/Makefile.am" ]; then
+    echo "Error: test submodule (${tests_sos_dir}) contents are missing."
+    echo "Please run the following command to download the SOS tests:"
+    echo "   git submodule update --init"
+    exit 1
+else
+    if [ -d "$current_dir/test" ] && [ ! -z "$(ls -A "$current_dir/test")" ]; then
+        echo "The ./test directory already contains files."
+        read -p "Do you want to overwrite them? (y/n): " overwrite
+        if [ "$overwrite" != "y" ]; then
+            echo "Copying tests-sos submodule aborted. Continuing..."
+        fi
+    fi
+    cp -r "${tests_sos_dir}/test" "$current_dir"
+    if [ $? -eq 0 ]; then
+        echo "tests-sos submodule copied successfully."
+    else
+        echo "Error: tests-sos submodule copy failed."
+        exit 1;
+    fi
 fi
 
 FILES=./man/*.1
