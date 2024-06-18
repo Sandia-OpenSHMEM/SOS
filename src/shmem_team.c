1c4c250686b2b88c728eb87172f923539723ad61
Ozog, David M <david.m.ozog@intel.com>
2024-06-17 18:13:46 -0700

    teams: if split stride is zero, force it to be 1

--- src/shmem_team.c
+++ src/shmem_team.c
@@ -332,7 +332,7 @@ int shmem_internal_team_split_strided(shmem_internal_team_t *parent_team, int PE
 
         myteam->my_pe       = my_pe;
         myteam->start       = global_PE_start;
-        myteam->stride      = PE_stride;
+        myteam->stride      = (PE_stride == 0 || PE_size == 1) ? 1 : PE_stride;
         myteam->size        = PE_size;
 
         if (config_mask == 0) {
@@ -424,7 +424,7 @@ int shmem_internal_team_split_strided(shmem_internal_team_t *parent_team, int PE
     /* If no team was available, print some team triplet info and return nonzero. */
     if (my_pe >= 0 && myteam != NULL && myteam->psync_idx == -1) {
         RAISE_WARN_MSG("Team split strided failed: child <%d, %d, %d>, parent <%d, %d, %d>\n",
-                        global_PE_start, PE_stride, PE_size,
+                        myteam->start, myteam->stride, myteam->size,
                         parent_team->start, parent_team->stride, parent_team->size);
     }
     return *team_ret_val_reduced;
