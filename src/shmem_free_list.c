6a369b2a3a7b23dcb8884d988fdbfcb3d64d6f55
Mark F. Brown <mark.f.brown@intel.com>
2026-01-27 13:20:36 -0500

    Allocate Bounce Buffer from Symmetric Heap
    Issue #1221
    
    Signed-off-by: Mark F. Brown <mark.f.brown@intel.com>

--- src/shmem_free_list.c
+++ src/shmem_free_list.c
@@ -22,10 +22,12 @@
 #include "shmem.h"
 #include "shmem_free_list.h"
 
+#define NUM_ELEMENTS	2
 
 shmem_free_list_t*
-shmem_free_list_init(unsigned int element_size,
-                     shmem_free_list_item_init_fn_t init_fn)
+shmem_free_list_init(size_t element_size,
+                     shmem_free_list_item_init_fn_t init_fn,
+		     size_t max_pool_cnt)
 {
     int ret;
     shmem_free_list_t *fl = (shmem_free_list_t*) calloc(1, sizeof(shmem_free_list_t));
@@ -34,6 +36,18 @@ shmem_free_list_init(unsigned int element_size,
     fl->element_size = element_size;
     fl->init_fn = init_fn;
     fl->nalloc = 0;
+    fl->alloc_size = sizeof(shmem_free_list_alloc_t) + NUM_ELEMENTS * fl->element_size;
+    fl->pool_size = 0;
+    fl->pool_ofs = 0;
+    fl->pool = NULL;
+    if (max_pool_cnt) {
+        /* preallocate pool with shmem malloc */
+        /* memory must be reserved as a memory pool to prevent address conflicts between PEs */
+        fl->pool_size = fl->alloc_size * max_pool_cnt;
+        fl->pool = shmem_internal_shmalloc(fl->pool_size);
+    }
+    /* if pool count is zero allocate with bounce buffers with malloc */
+
     SHMEM_MUTEX_INIT(fl->lock);
     ret = shmem_free_list_more(fl);
     if (0 != ret) {
@@ -48,13 +62,20 @@ shmem_free_list_init(unsigned int element_size,
 void
 shmem_free_list_destroy(shmem_free_list_t *fl)
 {
-    shmem_free_list_alloc_t *alloc, *next;
-
-    alloc = fl->allocs;
-    while (NULL != alloc) {
-        next = alloc->next;
-        free(alloc);
-        alloc = next;
+    if (fl->pool) {
+        /* allocated with shmem malloc */
+        shmem_internal_free(fl->pool);
+        fl->pool = NULL;
+        fl->pool_ofs = 0;
+    } else {
+        /* allocated with malloc */
+        shmem_free_list_alloc_t *alloc, *next;
+        alloc = fl->allocs;
+        while (NULL != alloc) {
+            next = alloc->next;
+            free(alloc);
+            alloc = next;
+        }
     }
 
     SHMEM_MUTEX_DESTROY(fl->lock);
@@ -64,25 +85,35 @@ shmem_free_list_destroy(shmem_free_list_t *fl)
 int
 shmem_free_list_more(shmem_free_list_t *fl)
 {
-    size_t page_size = 4096 - sizeof(shmem_free_list_alloc_t);
-    int num_elements = (fl->element_size < page_size) ? page_size / fl->element_size : 1;
     shmem_free_list_item_t *item, *first, *next, *last = NULL;
     shmem_free_list_alloc_t *header;
     char *buf;
-    int i;
+    uint64_t i;
 
-    num_elements = 2;
+    if (fl->pool) {
+       if (fl->pool_ofs >= fl->pool_size) {
+           fprintf(stderr, "[%d] pool memory exhausted\n", shmem_internal_my_pe);
+           return 1;
+       }   
 
-    buf = malloc(sizeof(shmem_free_list_alloc_t) +
-                 num_elements * fl->element_size);
-    if (NULL == buf) return 1;
+       buf = &fl->pool[fl->pool_ofs];
+       fl->pool_ofs += fl->alloc_size;
+
+       if (shmem_internal_params.BOUNCE_MLOCK) {
+           mlock(buf, sizeof(shmem_free_list_alloc_t) + NUM_ELEMENTS * fl->element_size);
+       }
+    } else {
+        buf = malloc(sizeof(shmem_free_list_alloc_t) +
+                     NUM_ELEMENTS * fl->element_size);
+        if (NULL == buf) return 1;
+    }
 
     header = (shmem_free_list_alloc_t*) buf;
     first = item = (shmem_free_list_item_t*) (header + 1);
-    for (i = 0 ; i < num_elements ; ++i) {
+    for (i = 0 ; i < NUM_ELEMENTS ; ++i) {
         fl->init_fn(item);
         next = (shmem_free_list_item_t*)((char*)item + fl->element_size);
-        if (i == num_elements - 1) {
+        if (i == NUM_ELEMENTS - 1) {
             item->next = NULL;
             last = item;
         } else {
